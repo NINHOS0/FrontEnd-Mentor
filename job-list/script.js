a6888db28d5d6ff984b5ee9871078ba42e9e7459
Renan Granzotti <renanpgranzotti@gmail.com>
2023-10-25 18:52:42 -0300

    Update

--- job-list/script.js
+++ job-list/script.js
@@ -1,104 +1,97 @@
+const clearBtn = document.getElementById("clearFilter");
+const itemParent = document.getElementById("itemList");
+const filterParent = document.getElementById("filters");
+const filterContent = document.getElementById("filterContent");
 
-let jobList;
-getJSON().then((data) => {
-  jobList = data;
-  createComponent()
+let data;
+let filter = [];
+
+window.addEventListener("load", async () => {
+  await fetch("data.json")
+    .then((res) => res.json())
+    .then((v) => (data = v))
+    .catch((err) => console.log(err));
+  loadData();
 });
-async function getJSON(){
-    let json = await fetch("./data.json")
-    .then((res) => res.json());
-    return json;
-}
 
-function getTags(element) {
-    let tags = [];
-    tags.push(element.role);
-    tags.push(element.level);
-    element.languages.forEach(v => tags.push(v));
-    element.tools.forEach(v => tags.push(v));
-    return tags;
-}
-
-function createComponent() {
-  document.querySelector(".jobList").innerHTML = ''.trim();
-  let jobsComponent = ''.trim();
-
-  jobList.forEach(job => {
-    const tags = getTags(job)
-    
-    if (filter.length > 0 && !inFilter(tags)) return;
-    
-    let tagsComponent = "";
-    tags.forEach(element => {
-        tagsComponent += `<span class="tag" onclick="setFilter('${element}')">${element}</span> \n`;
-    });
-    
-    jobsComponent += `
-    <div class="item">
-    <img src="${job.logo}" alt="Logo ${job.company}" class="company-img">
-    <div class="description">
-    <div class="top-desc">
-    <span class="company">${job.company}</span>
-    ${(job.new) && '<span class="new">new!</span>' || "".trim()}
-    ${(job.featured) && '<span class="featured">featured</span>' || "".trim()}
-    </div>
-    <div class="mid-desc">
-    <span class="title">${job.position}</span>
-    </div>
-    <div class="bottom-desc"> ${job.postedAt} <span class="dot"></span> ${job.contract} <span class="dot"></span> ${job.location} </div>
-    </div>
-    <hr>
-    <div class="tags">
-    ${tagsComponent.trim()}
-    </div>
-    </div>`;
-  }) 
-  document.querySelector(".jobList").innerHTML = jobsComponent;
+const loadData = () => {
+  data.forEach((e) => {
+    if (filter.length > 0) {
+      let founds = 0;
+      filter.forEach((f, i) => [e.role, e.level, ...e.tools, ...e.languages].includes(f) && founds++);
+      if (founds === filter.length) createItem(e);
+    } else createItem(e);
+  });
+  if (filter.length > 0) {
+    filter.forEach((e) => createFilter(e));
+  }
 };
 
-const filter = [];
-function setFilter(filter_){
-  if (filter.indexOf(filter_) != -1) return;
-  filter.push(filter_);
-  updateFilter();
-  if (filter.length === 1) document.querySelector(".filter-container").classList.remove("hidden");
-;}
-
-function removeFilter(filter_){
-  filter.splice(filter.indexOf(filter_), 1);
-  updateFilter();
-  if (filter.length === 0) document.querySelector(".filter-container").classList.add("hidden");
+function reloadData() {
+  itemParent.innerHTML = null;
+  filterParent.innerHTML = null;
+  console.log(filterContent);
+  if (!filter.length > 0) filterContent.classList.add("hidden");
+  else filterContent.classList.remove("hidden");
+  loadData();
 }
 
-function clearFilter(){
-  filter.splice(0, filter.length);
-  updateFilter()
-  document.querySelector(".filter-container").classList.add("hidden");
-}
+const createItem = (cont) => {
+  const _filters = [cont.role, cont.level, ...cont.languages, ...cont.tools];
+  let _filterString = "";
 
-function inFilter(data){
-  let i = 0
-  for (let index = 0; index < data.length; index++) {
-    const element = data[index];
-    if (filter.includes(element)) {
-      i++;
-    };
-  };
-  if (filter.length === i) return true;
-  else return false;
-}
-
-function updateFilter() {
-  let filterComp = ""
-  filter.map(element => {
-    filterComp += `
-      <div class="filter">
-        <span class="f-name">${element}</span>
-        <span class="f-btn" onclick="removeFilter('${element}')">
-          <img src="images/icon-remove.svg" alt="Remove filter">
+  const container = `
+    <div class="itemContent${cont.new ? " new" : ""}">
+      <div class="accImage">
+        <img src="${cont.logo}" alt="${cont.company}">
+      </div>
+      <div class="infoAccount">
+        <span class="nameContent">
+          <span class="name">${cont.company}</span>
+          ${cont.new ? '<span class="new">NEW!</span>' : ""}
+          ${cont.new ? '<span class="featured">FEATURED</span>' : ""}
         </span>
-      </div>`;
-  });
-  document.querySelector(".filters").innerHTML = filterComp;
-  createComponent()
+        <span class="details">
+          ${cont.position}
+        </span>
+        <span class="extras">
+          ${cont.postedAt} <span>·</span> ${cont.contract} <span>·</span> ${cont.location}
+        </span>
+      </div>
+      <hr>
+      <div class="filters">${_filters.map((f) => {
+        _filterString += `<span class="filterAccount" onclick="addFilter('${f.toString()}')">${f}</span>`;
+        if (_filters[_filters.length - 1] === f) return _filterString;
+      })}</div>
+    </div>
+  `;
+
+  itemParent.innerHTML += container.replaceAll(",", "");
+};
+
+const createFilter = (filt) => {
+  const container = `
+    <div class="filterItem">
+      <span class="title">${filt}</span>
+      <span class="remove" onclick="removeFilter('${filt}')"><img src="images/icon-remove.svg" alt="Remove filters" /></span>
+    </div>
+  `;
+
+  filterParent.innerHTML += container.replaceAll(",", "");
+};
+
+function addFilter(f) {
+  if (filter.includes(f)) return
+  filter.push(f)
+  reloadData();
 }
+
+function removeFilter(f) {
+  filter = filter.filter((e) => e !== f);
+  reloadData();
+}
+
+clearBtn.addEventListener("click", () => {
+  filter = [];
+  reloadData();
+});
